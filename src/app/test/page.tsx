74946a0f8c8968597c54bc9529a04a915465c5a4
“Jakob <jnunnendorf@icloud.com>
2023-05-29 00:12:06 +0200

    feat(app): add supabase
    1) added supabase to tthe dependencies
    
    2) created supabase client in src/api/supabaseClient.js
    
    3) created src/app/test/page.tsx to test the database connection

---
+++ src/app/test/page.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import Image from "next/image"
+import { useEffect, useState } from "react"
+import supabase from "src/api/supabaseClient"
+
+interface Smoothie {
+    id: number
+    created_at: string
+    title: string
+    method: string
+    rating: number
+    image: string
+}
+interface Product {
+    id: number
+    created_at: string
+    title: string
+    image: string
+    price: number
+}
+
+export default function Page() {
+    const [fetchError, setFetchError] = useState<string | null>(null)
+    const [products, setProducts] = useState<Product[] | null>(null)
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const { data, error } = await supabase.from("products").select("*")
+
+            if (error) {
+                setFetchError(error.message)
+                setProducts(null)
+                console.log(error)
+            } else if (data) {
+                setProducts(data as Product[])
+                setFetchError(null)
+            }
+        }
+
+        fetchProducts()
+    }, [])
+
+    return (
+        <div>
+            {fetchError && <p>{fetchError}</p>}
+            {products && (
+                <div className='grid grid-cols-3 py-12 gap-8'>
+                    {products.map((product, index) => (
+                        <div key={index} className='text-center flex flex-col items-center space-y-3'>
+                            <img src={product.image} alt="test" className='w-48'/>
+                        <h2 className='text-xl'>{product.title}</h2>
+                        <p className='text-xl'>{product.price}€</p>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    )
+}
