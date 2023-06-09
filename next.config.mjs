bbbc6518ad19095739a19374c66ca9309a159fd2
“Jakob <jnunnendorf@icloud.com>
2023-06-09 16:36:20 +0800

    fix(error): fix the .env not found error
    because the next.config file used .env before i had to change it so that it doesnt

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,28 @@
 import withBundleAnalyzer from "@next/bundle-analyzer"
 import withPlugins from "next-compose-plugins"
-import { env } from "./env.mjs"
 
 /**
  * @type {import('next').NextConfig}
  */
-const config = withPlugins([[withBundleAnalyzer({ enabled: env.ANALYZE })]], {
-  reactStrictMode: true,
-  experimental: { instrumentationHook: true },
-  rewrites() {
-    return [
-      { source: "/healthz", destination: "/api/health" },
-      { source: "/api/healthz", destination: "/api/health" },
-      { source: "/health", destination: "/api/health" },
-      { source: "/ping", destination: "/api/health" },
-    ]
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
+const config = withPlugins([[withBundleAnalyzer({ enabled: false })]], {
+    reactStrictMode: true,
+    experimental: { instrumentationHook: true },
+    rewrites() {
+        return [
+            { source: "/healthz", destination: "/api/health" },
+            { source: "/api/healthz", destination: "/api/health" },
+            { source: "/health", destination: "/api/health" },
+            { source: "/ping", destination: "/api/health" },
+        ]
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+            },
+        ],
+    },
 })
 
 export default config
