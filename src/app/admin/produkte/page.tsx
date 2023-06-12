30340d8d35d8ccaf1dfcc335be2f4948b8950981
“Jakob <jnunnendorf@icloud.com>
2023-06-13 01:51:29 +0800

    feat(admin): design product management page

--- src/app/admin/produkte/page.tsx
+++ src/app/admin/produkte/page.tsx
@@ -1,45 +1,37 @@
 import { Metadata } from "next"
-import Image from "next/image"
+import ProductCard from "src/components/ProductCard/ProductCard"
+import { iProductFetchResponse } from "src/types/fetchResponseTypes"
+import { iProduct } from "src/types/ProductTypes"
+import { iUserRole } from "src/types/UserTypes"
 
 export const metadata: Metadata = {
     title: "",
     description: "",
 }
 
-export default function ProductManagementPage() {
-    const products = [1, 2, 3, 4, 5, 6]
+
+
+async function fetchAllProducts() {
+    const fetchResponse = await fetch("http://127.0.0.1:3658/m1/367756-0-default/getAllProducts")
+    if (!fetchResponse.ok) {
+        // This will activate the closest `error.js` Error Boundary
+        throw new Error("Failed to fetch data")
+    }
+    const responseJson = await fetchResponse.json()
+    const productFetchResponse = responseJson as iProductFetchResponse
+    return productFetchResponse
+}
+
+export default async function ProductManagementPage() {
+    const fetchResponse: iProductFetchResponse = await fetchAllProducts()
+    const productDataArray: iProduct[] = fetchResponse.data
     const ProductManagementPageContent = (
         <section className="space-y-4">
-            <h1 className="text-3xl text-center">Produkte verwalten</h1>
-            <div className="flex-col items-center justify-center w-full p-4 border-2 rounded-3xl">
-                <table>
-                    <thead>
-                        <tr className="">
-                            <th className=""></th>
-                            <th className="px-1 border-2">ID</th>
-                            <th className="px-1 border-2">Produktname</th>
-                            <th className="px-1 border-2">Beschreibung</th>
-                            <th className="px-1 border-2">Preis</th>
-                            <th className="px-1 border-2">Kategorie</th>
-                            <th className="px-1 border-2">Bestand</th>
-                            <th className="px-1 border-2">Edit</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className="border-2">
-                            <td className="">
-                                <Image src='/p2d_logo.png' width={35} height={35} alt=''/>
-                            </td>
-                            <td className="border-2">a2fc-45bb</td>
-                            <td className="border-2">iPhone 13 Hülle schwarz</td>
-                            <td className="border-2">Silikon Hülle für optimalen Schutz</td>
-                            <td className="border-2">13,49€</td>
-                            <td className="border-2">Handyhülle</td>
-                            <td className="border-2">37</td>
-                            <td className="border-2">...</td>
-                        </tr>
-                    </tbody>
-                </table>
+            <h1 className="text-center text-3xl">Produkte verwalten</h1>
+            <div className="min-w-32 grid grid-cols-4 gap-4 rounded-3xl border-2 p-4 xl:grid-cols-5">
+                {productDataArray.map((product) => {
+                    return <ProductCard product={product} role={iUserRole.admin} key={product.id} />
+                })}
             </div>
         </section>
     )
