50e216723947fa9f21447a2473b53108231bd4e0
“Jakob <jnunnendorf@icloud.com>
2023-06-13 18:03:57 +0800

    feat(admin): fetch products from supabase

--- src/app/admin/produkte/page.tsx
+++ src/app/admin/produkte/page.tsx
@@ -1,15 +1,9 @@
-import { Metadata } from "next"
 import ProductCard from "src/components/ProductCard/ProductCard"
 import { iProductFetchResponse } from "src/types/fetchResponseTypes"
 import { iProduct } from "src/types/ProductTypes"
 import { iUserRole } from "src/types/UserTypes"
-
-export const metadata: Metadata = {
-    title: "",
-    description: "",
-}
-
-
+import supabase from "utils/supabase"
+import Push2DB from "./Push2DB"
 
 async function fetchAllProducts() {
     const fetchResponse = await fetch("http://127.0.0.1:3658/m1/367756-0-default/getAllProducts")
@@ -22,16 +16,23 @@ async function fetchAllProducts() {
     return productFetchResponse
 }
 
+export const revalidate = 0
+
 export default async function ProductManagementPage() {
     const fetchResponse: iProductFetchResponse = await fetchAllProducts()
+    const { data: names } = await supabase.from("names").select()
+    const { data: products } = await supabase.from("products").select()
+
+
     const productDataArray: iProduct[] = fetchResponse.data
     const ProductManagementPageContent = (
         <section className="space-y-4">
-            <h1 className="text-center text-3xl">Produkte verwalten</h1>
-            <div className="min-w-32 grid grid-cols-4 gap-4 rounded-3xl border-2 p-4 xl:grid-cols-5">
-                {productDataArray.map((product) => {
+            <h1 className="text-3xl text-center">Produkte verwalten</h1>
+            <div className="grid grid-cols-4 gap-4 p-4 border-2 min-w-32 rounded-3xl xl:grid-cols-5">
+                {products.map((product) => {
                     return <ProductCard product={product} role={iUserRole.admin} key={product.id} />
                 })}
+            <Push2DB />
             </div>
         </section>
     )
