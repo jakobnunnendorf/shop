245ad28b12a7e1742b7c42d50ab9ffdd5430eed7
“Jakob <jnunnendorf@icloud.com>
2023-06-16 22:50:26 +0800

    refactor(usernavmenu): just route straight to user page

--- src/app/Header/UserNavMenu/UserNavMenu.tsx
+++ src/app/Header/UserNavMenu/UserNavMenu.tsx
@@ -1,20 +1,16 @@
 "use client"
 
+import Link from 'next/link'
 import React, { useState } from "react"
 import { FiUser } from "react-icons/fi"
 import UserNavLinks from "./UserNavLinks"
 
 export default function UserNavMenu() {
-    const [showNavLinks, setShowNavLinks] = useState(false)
-    function toggleNavLinks() {
-        setShowNavLinks((showNavLinks) => !showNavLinks)
-    }
     return (
-        <nav className="relative flex h-16 w-12 items-center">
-            <button onClick={() => toggleNavLinks()}>
+        <nav className="relative flex items-center w-12 h-16">
+            <Link href='/user'>
                 <FiUser size={32} />
-            </button>
-            {showNavLinks && <UserNavLinks />}
+            </Link>
         </nav>
     )
 }
