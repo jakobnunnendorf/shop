89782678bff144867fd6df00782a187dec3cfea8
“Jakob <jnunnendorf@icloud.com>
2023-06-16 22:45:43 +0800

    chore(types): change to d.ts file

--- src/types/UserTypes.d.ts
+++ src/types/UserTypes.d.ts
@@ -1,34 +1,34 @@
-import { UUID } from "crypto"
+
+import { UUID } from 'crypto';
 
 interface iReview {
-    reviewID: UUID
-    rating: number
-    createdAt: Date
-    comment: string
-    userID: UUID
-    productID: UUID
+    reviewID: UUID;
+    rating: number;
+    createdAt: Date;
+    comment: string;
+    userID: UUID;
+    productID: UUID;
 }
 
 interface iUserDetails {
-    avatar: null | string
-    orders: iOrder[] | null
-    bewertungen: iReview | null
-    email: string
-    firstName: string
-    lastName: null | string
-    password: string
-    defaultShippingAdress: null | iAdress
-    defaultBillingAdress: null | iAdress
-    phoneNumber: null | string
-    userID: UUID
-    wishList: iProduct[] | null
-    settings: iSettings
+    avatar: null | string;
+    orders: iOrder[] | null;
+    bewertungen: iReview | null;
+    email: string;
+    firstName: string;
+    lastName: null | string;
+    password: string;
+    defaultShippingAdress: null | iAdress;
+    defaultBillingAdress: null | iAdress;
+    phoneNumber: null | string;
+    userID: UUID;
+    wishList: iProduct[] | null;
+    settings: iSettings;
 }
 
 interface iSettings {
-    darkMode: boolean
+    darkMode: boolean;
 }
-
 interface iProductReview {
     reviewID: string
     rating: number
