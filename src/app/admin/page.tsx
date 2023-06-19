f53201ec078f555da143949a2ce548fd4026a127
“Jakob <jnunnendorf@icloud.com>
2023-06-20 03:16:26 +0800

    feat(admin): add route protection

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,14 +1,35 @@
-export default function AdminDashboard() {
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+export default async function AdminDashboard() {
+    const supabase = createServerComponentClient({ cookies });
+    const {
+        data: { session },
+    } = await supabase.auth.getSession();
+
+    if (!session || session.user.role !== 'admin_p2d') {
+        redirect('/');
+    }
+
     const AdminDashboardContent = (
-        <div className="flex flex-col items-center w-full space-y-8">
-            <h1 className="w-full text-4xl text-center">Admin Dashboard</h1>
-            <div className="grid grid-cols-2 grid-rows-2 gap-8 w-fit">
-                <div className="flex items-center justify-center h-48 border-2 w-80 rounded-3xl">Kunden</div>
-                <div className="flex items-center justify-center h-48 border-2 w-80 rounded-3xl">Produkte</div>
-                <div className="flex items-center justify-center h-48 border-2 w-80 rounded-3xl">Bestellungen</div>
-                <div className="flex items-center justify-center h-48 border-2 w-80 rounded-3xl">Einstellungen</div>
+        <div className='flex w-full flex-col items-center space-y-8'>
+            <h1 className='w-full text-center text-4xl'>Admin Dashboard</h1>
+            <div className='grid w-fit grid-cols-2 grid-rows-2 gap-8'>
+                <div className='flex h-48 w-80 items-center justify-center rounded-3xl border-2'>
+                    Kunden
+                </div>
+                <div className='flex h-48 w-80 items-center justify-center rounded-3xl border-2'>
+                    Produkte
+                </div>
+                <div className='flex h-48 w-80 items-center justify-center rounded-3xl border-2'>
+                    Bestellungen
+                </div>
+                <div className='flex h-48 w-80 items-center justify-center rounded-3xl border-2'>
+                    Einstellungen
+                </div>
             </div>
         </div>
-    )
-    return AdminDashboardContent
+    );
+    return AdminDashboardContent;
 }
