c3d9a738de15e6ddd4a8017326355c72e19eadb6
“Jakob <jnunnendorf@icloud.com>
2023-06-20 21:21:35 +0800

    feat(admin): protect route

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -8,36 +8,39 @@ export default async function AdminDashboard() {
     const {
         data: { session },
     } = await supabase.auth.getSession();
+    const { data: userRole, error } = await supabase
+        .from('profiles')
+        .select('role');
 
-    if (!session || session.user.role !== 'admin_p2d') {
+    if (!session || userRole[0].role !== 'admin') {
         redirect('/');
     }
 
     const AdminDashboardContent = (
-        <div className='flex w-full flex-col items-center space-y-8'>
-            <h1 className='w-full text-center text-4xl'>Admin Dashboard</h1>
-            <div className='grid w-fit grid-cols-2 grid-rows-2 gap-8'>
+        <div className='flex flex-col items-center w-full space-y-8'>
+            <h1 className='w-full text-4xl text-center'>Admin Dashboard</h1>
+            <div className='grid grid-cols-2 grid-rows-2 gap-8 w-fit'>
                 <Link
                     href='/admin/kunden'
-                    className='flex h-48 w-80 items-center justify-center rounded-3xl border-2'
+                    className='flex items-center justify-center h-48 border-2 w-80 rounded-3xl'
                 >
                     Kunden
                 </Link>
                 <Link
                     href='admin/produkte'
-                    className='flex h-48 w-80 items-center justify-center rounded-3xl border-2'
+                    className='flex items-center justify-center h-48 border-2 w-80 rounded-3xl'
                 >
                     Produkte
                 </Link>
                 <Link
                     href='/admin/bestellungen'
-                    className='flex h-48 w-80 items-center justify-center rounded-3xl border-2'
+                    className='flex items-center justify-center h-48 border-2 w-80 rounded-3xl'
                 >
                     Bestellungen
                 </Link>
                 <Link
                     href='/admin/einstellungen'
-                    className='flex h-48 w-80 items-center justify-center rounded-3xl border-2'
+                    className='flex items-center justify-center h-48 border-2 w-80 rounded-3xl'
                 >
                     Einstellungen
                 </Link>
