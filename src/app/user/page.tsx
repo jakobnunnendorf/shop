79ac89d482997a26f23f504f12b6f7292e2df618
“Jakob <jnunnendorf@icloud.com>
2023-06-22 15:55:13 +0800

    fix(user page): try withoud window reload

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,3 +1,22 @@
-export default function UserPage() {
-    return <div>userpage</div>
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import Login from './Login/Login';
+import Account from './profile/Profile';
+
+export default async function UserPage() {
+    const supabase = createServerComponentClient({ cookies });
+
+    const {
+        data: { session: currentSession },
+    } = await supabase.auth.getSession();
+
+    if (!currentSession) {
+        return <Login />;
+    } else {
+        return (
+            <div>
+                <Account session={currentSession} />
+            </div>
+        );
+    }
 }
