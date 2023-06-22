ae45fc0feb26c6c2bafd6d262ca7dee15bec18a9
“Jakob <jnunnendorf@icloud.com>
2023-06-22 15:37:50 +0800

    fix(user): test removing db logic

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -1,13 +1,3 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
-import Link from 'next/link';
-import {
-    FiCompass,
-    FiHeart,
-    FiPackage,
-    FiSettings,
-    FiUser,
-} from 'react-icons/fi';
 
 export const dynamic = 'force-dynamic';
 
@@ -16,128 +6,11 @@ export const metadata = {
     description: 'description_string',
 };
 
-export default async function UserLayout({
+export default function UserLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
-    const supabase = createServerComponentClient({ cookies });
-    const {
-        data: { session: currentSession },
-    } = await supabase.auth.getSession();
-    const user = currentSession?.user;
-    const {
-        data: profileData,
-        error: profileFetchError,
-        status: profileFetchStatus,
-    } = await supabase
-        .from('profiles')
-        .select()
-        .eq('profile_id', user?.id)
-        .single();
-    const userRole = profileData?.role;
-
-    const linkedPages = [
-        {
-            route: '/user',
-            title: 'Mein Profil',
-            key: 'profile',
-            icon: <FiUser className='text-slate-500' size={30} />,
-        },
-        {
-            route: '/user/bestellungen',
-            title: 'Bestellungen',
-            key: 'orders',
-            icon: <FiPackage className='text-slate-500' size={30} />,
-        },
-        {
-            route: '/user/wunschliste',
-            title: 'Wunschliste',
-            key: 'wishlist',
-            icon: <FiHeart className='text-slate-500' size={30} />,
-        },
-        {
-            route: '/user/einstellungen',
-            title: 'Einstellungen',
-            key: 'settings',
-            icon: <FiSettings className='text-slate-500' size={30} />,
-        },
-    ];
-    const navigationMenu = (
-        <ul className='flex flex-col items-center justify-around w-full mt-8 space-y-8 h-fit lg:items-start'>
-            {userRole === 'admin' && (
-                <Link href='/admin'>
-                    <li className='flex items-center space-x-2 '>
-                        <span className=''>
-                            <FiCompass className='text-slate-500' size={30} />
-                        </span>
-                        <span className='hidden text-xl lg:inline'>Admin</span>
-                    </li>
-                </Link>
-            )}
-            {linkedPages.map((page) => (
-                <Link href={page.route} key={page.route}>
-                    <li className='flex items-center space-x-2 '>
-                        <span className=''>{page.icon}</span>
-                        <span className='hidden text-xl lg:inline'>
-                            {page.title}
-                        </span>
-                    </li>
-                </Link>
-            ))}
-        </ul>
-    );
-    const mobileMenu = (
-        <nav className='fixed bottom-0 grid h-28 w-full list-none grid-flow-col rounded-t-3xl bg-slate-700 py-4 text-xs font-bold text-white lg:hidden'>
-            <Link href='/user'>
-                <li className='flex flex-col items-center space-y-2'>
-                    <FiUser className='text-slate-300' size={30} />
-                    <div>Profil</div>
-                </li>
-            </Link>
-            <Link href='/user/bestellungen'>
-                <li className='flex flex-col items-center space-y-2'>
-                    <FiPackage className='text-slate-300' size={30} />
-                    <div>Bestellungen</div>
-                </li>
-            </Link>
-            {userRole === 'admin' && (
-                <Link href='/admin'>
-                    <li className='flex flex-col items-center space-y-2'>
-                        <FiCompass className='text-slate-300' size={30} />
-                        <div>Admin</div>
-                    </li>
-                </Link>
-            )}
-            <Link href='/user/wunschliste'>
-                <li className='flex flex-col items-center space-y-2'>
-                    <FiHeart className='text-slate-300' size={30} />
-                    <div>Wunschliste</div>
-                </li>
-            </Link>
-            <Link href='/user/einstellungen'>
-                <li className='flex flex-col items-center space-y-2'>
-                    <FiSettings className='text-slate-300' size={30} />
-                    <div>Einstellungen</div>
-                </li>
-            </Link>
-        </nav>
-    );
-    const AdminLayout = (
-        <section className='flex flex-col lg:flex-row'>
-            {currentSession && (
-                <aside className='hidden h-screen w-12 bg-slate-100 lg:block lg:w-fit lg:px-6 lg:py-12'>
-                    <h2 className='text-2xl font-bold text-coastal-blue-10'>
-                        Mein Account
-                    </h2>
-                    {navigationMenu}
-                </aside>
-            )}
-            <section className='w-full px-2 py-8 pb-44 lg:px-16 lg:pb-8'>
-                {children}
-            </section>
-            {mobileMenu}
-        </section>
-    );
-    return AdminLayout;
+   
+    return <div>user layout {children}</div>;
 }
--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,28 +1,3 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
-import Login from './Login/Login';
-import Account from './profile/Profile';
-
-export default async function UserPage() {
-    const supabase = createServerComponentClient({ cookies });
-
-    const {
-        data: { session: currentSession },
-    } = await supabase.auth.getSession();
-    // refresh page on auth change
-    supabase.auth.onAuthStateChange((_, session) => {
-        if (!session) {
-            window.location.reload();
-        }
-    });
-
-    if (!currentSession) {
-        return <Login />;
-    } else {
-        return (
-            <div>
-                <Account session={currentSession} />
-            </div>
-        );
-    }
+export default function UserPage() {
+    return <div>userpage</div>
 }
