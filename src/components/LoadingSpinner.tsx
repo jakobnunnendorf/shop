f1593c874e50f5f18cac99229b217fd6098299de
“Jakob <jnunnendorf@icloud.com>
2023-06-25 14:41:19 +0800

    feat(components): add a loading spinner

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function LoadingSpinner() {
+    return (
+        <div className=' flex h-[75vh] w-full flex-col items-center justify-center'>
+            <img src='/loading.gif' alt='' />
+            <h2 className='text-3xl font-bold text-coastal-blue-9'>
+                Einen Moment...
+            </h2>
+        </div>
+    );
+}
