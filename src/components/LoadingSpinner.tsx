6d62dd8211eecd95910dc70867e6f305128eed5a
“Jakob <jnunnendorf@icloud.com>
2023-07-20 14:43:32 +0800

    feat(loading): add loading screens

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -4,9 +4,15 @@ import React from 'react';
 export default function LoadingSpinner() {
     return (
         <div className=' flex h-[75vh] w-full flex-col items-center justify-center'>
-            <Image src='/loading.gif' alt='' />
-            <h2 className='text-3xl font-bold text-coastal-blue-9'>
-                Einen Moment...
+            <figure className='relative h-48 w-48'>
+                <Image src='/loading2.gif' alt='' fill />
+            </figure>
+            <h3 className='text-center text-2xl font-bold text-sandy-beige-9'>
+                Alles, <br />
+                was dein Handy braucht
+            </h3>
+            <h2 className='text-center text-3xl font-bold text-coastal-blue-9'>
+                Phone 2 Door
             </h2>
         </div>
     );
