0bc1e64fb59a46fe69883f7966e1bbf6798dd1f3
“Jakob <jnunnendorf@icloud.com>
2023-07-22 00:47:14 +0800

    fix(productcard): make expanded property local
    otherwise all cards are opened on click

--- src/components/ProductCard/Collapsed/Collapsed.tsx
+++ src/components/ProductCard/Collapsed/Collapsed.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import {
+    ActiveProductContext,
+    ActiveProductContextType,
+} from '@globalState/ActiveProductCardContext';
 import CollapsedInfo from './CollapsedInfo';
 import CollapsedPicture from './CollapsedPicture';
 
-export default function Collapsed({ product }: { product: product }) {
+export default function Collapsed({
+    product,
+    setExpanded,
+}: {
+    product: product;
+    setExpanded: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
     const imageURL = product.imageURL_object.default_color.imageURL_array[0];
 
     const collapsed = (
-        <div className='grid w-full h-full grid-rows-5 lg:grid-rows-2'>
-            <CollapsedPicture imageURL={imageURL} />
-            <CollapsedInfo product={product} />
+        <div className='grid h-full w-full grid-rows-5 lg:grid-rows-2'>
+            <CollapsedPicture imageURL={imageURL} setExpanded={setExpanded} />
+            <CollapsedInfo product={product} setExpanded={setExpanded} />
         </div>
     );
 
