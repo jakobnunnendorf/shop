dd2ece3f1c2d889a5227e54e270afdb424452e7a
“Jakob <jnunnendorf@icloud.com>
2023-07-22 12:01:52 +0800

    refactor(checkout routes): split routes and fix guest route

--- src/app/warenkorb/checkout/OnBoarding/GuestCheckout/GuestCheckout.tsx
+++ src/app/warenkorb/checkout/OnBoarding/GuestCheckout/GuestCheckout.tsx
@@ -4,36 +4,38 @@ import React from 'react';
 
 export default function GuestCheckout({ cart }: { cart: cart_item[] }) {
     const router = useRouter();
-    const handle_guest_checkout = async () => {
-        const data: any = await fetch('/api/checkout', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                cartItems: [...cart],
-                metadata: {
-                    checkout_mode: 'guest_checkout',
-                },
-            }),
-        });
-        const { url } = await data.json();
-        router.push(url);
-    };
+  const handle_guest_checkout = async () => {
+      const data: any = await fetch('/api/checkout/guest', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+              cartItems: [...cart],
+          }),
+      });
+      const { url, error } = await data.json();
+      if (!error) {
+          console.log(url);
+          router.push(url);
+      } else {
+          console.log(error);
+      }
+  };
 
     const guestCheckout = (
         <li className='flex flex-col items-start justify-center space-x-4'>
             {' '}
             <div className='flex items-center space-x-4'>
-                <div className='flex aspect-square h-8 w-8 items-center justify-center rounded-full border border-coastal-blue-10 p-2 font-bold text-coastal-blue-10'>
+                <div className='flex items-center justify-center w-8 h-8 p-2 font-bold border rounded-full aspect-square border-coastal-blue-10 text-coastal-blue-10'>
                     2
                 </div>{' '}
                 <h3>oder als Gast fortfahren</h3>
             </div>
-            <div className='col-span-2 flex w-full justify-center'>
+            <div className='flex justify-center w-full col-span-2'>
                 <button
                     onClick={handle_guest_checkout}
-                    className='relative mt-4 w-fit rounded-lg border border-coastal-blue-10 px-4 py-2 font-bold text-coastal-blue-10'
+                    className='relative px-4 py-2 mt-4 font-bold border rounded-lg w-fit border-coastal-blue-10 text-coastal-blue-10'
                 >
                     Gast Checkout
                 </button>
