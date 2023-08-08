a86e82aef70bd3c0afd30b4cf0952af97583fdee
MasterOz <hassanejaz400@gmail.com>
2023-08-08 14:42:20 +0500

    Pushing for further feature implementations

--- src/app/admin/benutzer/page.tsx
+++ src/app/admin/benutzer/page.tsx
@@ -1,65 +1,34 @@
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import Link from 'next/link';
-import UserInfoByID from '@app/user/profile/UserInfoPanel';
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 
-export default async function AdminDashboardUsersList({
-    profiles,
-}: {
-    profiles: profile[];
-}) {
+export default async function AdminDashboardUsersList() {
+
+    const supabase = createClientComponentClient();
+    
+    const user_profiles = async () => {
+        const {data: profiles} = await supabase
+        .from('profiles')
+        .select('*');
+
+        return profiles;
+    }
+    const profiles = await user_profiles() as profile[];
+
     return (
         <div>
-            <ul className='grid grid-cols-1 gap-2 p-2 md:grid-cols-1'>
-                {profiles.map((profile, index) => (
-                    <Link
-                        href={`/admin/benutzer/${profile.firstName}-${profile.lastName}`}
+            <ul className="grid grid-cols-1 gap-2 p-2 md:grid-cols-1">
+                    {profiles.map((profile, index) => (
+                    <Link 
+                        href={`/admin/benutzer/${profile.firstName}-${profile.lastName}`} 
                     >
-                        <li
-                            key={index}
-                            className='rounded-lg bg-white p-4 shadow-md'
-                            onClick={() => (
-                                <UserInfoByID user_id={profile.profile_id} />
-                            )}
-                        >
-                            <p className='text-4x1 antialiased hover:subpixel-antialiased'>
-                                {profile.firstName} {profile.lastName}
-                            </p>
+                        <li key={index} className="p-4 bg-white rounded-lg shadow-md" onClick = { () => <UserInfoByID user_id={profile.profile_id} /> } >
+                            <p className="antialiased text-4x1 hover:subpixel-antialiased">{profile.firstName} {profile.lastName}</p>
                         </li>
                     </Link>
                 ))}
             </ul>
         </div>
     );
-}
-
-export const getServerSideProps: GetServerSideProps = async () => {
-    const supabase = createClientComponentClient();
-
-    const fetchUsersProfiles = async () => {
-        const fetchUsers = async () => {
-            const { data: user_profiles } = await supabase
-                .from('profiles')
-                .select('*');
-
-            console.log(user_profiles);
-            return user_profiles;
-        };
-
-        const user_profiles = (await fetchUsers()) as profile[];
-
-        // filtering users with valid data
-        const filteredProfiles = filterProfilesByName(user_profiles);
-        return filteredProfiles;
-    };
-
-    const profiles = fetchUsersProfiles();
-
-    return {
-        props: {
-            profiles,
-        },
-    };
 };
 
 // ------------------ Helper Functions ------------------
