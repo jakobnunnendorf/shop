8de9790d7e08af4d4b4acefcd1c75053298f7cb4
MasterOz <hassanejaz400@gmail.com>
2023-08-09 10:53:23 +0500

    Moving towards Pagination

--- src/app/shop/FilterBar/FilterBar.tsx
+++ src/app/shop/FilterBar/FilterBar.tsx
@@ -1,16 +1,17 @@
-import Link from 'next/link';
 import React from 'react';
 import Client from './components/Client';
 import { modelTree, returnModelTree } from './helperFunctions';
+import Pagination from './Pagination';
 
 export default async function FilterBar() {
     const modelTree: modelTree = await returnModelTree();
-    
+
     const filterBar = (
         <div className='h-fit w-full border lg:flex lg:h-auto lg:min-h-screen lg:w-auto lg:flex-grow lg:flex-col'>
             <div className='hidden lg:block'>
                 <Client modelTree={modelTree} />
             </div>
+            <Pagination />
         </div>
     );
 
