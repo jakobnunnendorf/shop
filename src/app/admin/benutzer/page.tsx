73dd37b84902e7644f783eab8f47bbaef63e741c
Usman <i222562@nu.edu.pk>
2023-08-09 23:22:52 +0500

    Delete icon and functionality added in admin bentzure

--- src/app/admin/benutzer/page.tsx
+++ src/app/admin/benutzer/page.tsx
@@ -1,35 +1,52 @@
-
+'use client';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import Link from 'next/link';
+import { FiTrash2 } from 'react-icons/fi';
+import deleteUser from './DeleteUser';
+
+function handleClick(profile: profile) {
+    deleteUser(profile);
+}
 
 export default async function AdminDashboardUsersList() {
-
     const supabase = createClientComponentClient();
-    
+
     const user_profiles = async () => {
-        const {data: profiles} = await supabase
-        .from('profiles')
-        .select('*');
+        const { data: profiles } = await supabase.from('profiles').select('*');
 
         return profiles;
-    }
-    const all_profiles = await user_profiles() as profile[];
-    const profiles = filterProfilesByName(all_profiles);
+    };
+    const profiles = (await user_profiles()) as profile[];
 
     return (
-        <>
-            <ul className="grid grid-cols-1 md:grid-cols-1 gap-2 p-2">
+        <div>
+            <ul className='grid grid-cols-1 gap-2 p-2 md:grid-cols-1'>
                 {profiles.map((profile, index) => (
-                    <li key={index} className='bg-white rounded-lg shadow-md p2 md:p-4'>
-                        <Link href={`/admin/benutzer/${profile.profile_id}`} >
-                            <p className='text-coastal-blue font-semibold text-x1'>{profile.firstName} {profile.lastName}</p>
+                    <li
+                        key={index}
+                        className='w-400 flex rounded-lg bg-white p-4 shadow-md'
+                    >
+                        <Link
+                            href={`/admin/benutzer/${profile.profile_id}`}
+                            className='justify-left'
+                        >
+                            {profile.firstName} {profile.lastName}
                         </Link>
+                        <button
+                            className='ml-auto'
+                            onClick={() => {
+                                handleClick(profile);
+                                window.location.reload();
+                            }}
+                        >
+                            <FiTrash2 size={30} color='red' />
+                        </button>
                     </li>
                 ))}
             </ul>
-        </>
+        </div>
     );
-};
+}
 
 // ------------------ Helper Functions ------------------
 
@@ -41,6 +58,6 @@ const filterProfilesByName = (profiles: profile[]) => {
             return true;
         }
     });
-    
+
     return filteredUsers;
 };
