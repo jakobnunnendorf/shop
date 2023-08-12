3cace3a5651109269d72b09318872925a5fd82f1
“Jakob <jnunnendorf@icloud.com>
2023-08-12 17:25:14 +0800

    refactor(ProductCard): link to dynamic page

--- src/components/ProductCard/Collapsed/CollapsedInfo.tsx
+++ src/components/ProductCard/Collapsed/CollapsedInfo.tsx
@@ -3,26 +3,21 @@ import CompatibleModelTags from '@components/ProductCard/CompatibleModelTags';
 import Price from '@components/ProductCard/Price';
 import AddToCartButton from './AddToCartButton';
 import styles from './collapsedInfo.module.css';
-export default function CollapsedInfo({
-    product,
-    setExpanded,
-}: {
-    product: product;
-    setExpanded: React.Dispatch<React.SetStateAction<boolean>>;
-}) {
+import Link from 'next/link';
+export default function CollapsedInfo({ product }: { product: product }) {
     const collapsedInfo = (
-        <div className='row-span-3 flex w-full flex-col justify-between lg:row-span-1'>
-            <button
+        <div className='flex flex-col justify-between w-full row-span-3 lg:row-span-1'>
+            <Link
+                href={`/produkte/${product.id}`}
                 className={`text-md h-2/5 overflow-hidden px-2 pt-4 text-center font-bold lg:w-full lg:px-4 ${styles.dashboard}`}
-                onClick={() => setExpanded(true)}
             >
                 <h2>{product.title}</h2>
-            </button>
+            </Link>
             <CompatibleModelTags
                 compatibleModels_array={product.compatibleModels}
             />
 
-            <div className='flex items-center justify-evenly pb-4'>
+            <div className='flex items-center pb-4 justify-evenly'>
                 <Price productPrice={product.price} />
                 <AddToCartButton product={product} />
             </div>
