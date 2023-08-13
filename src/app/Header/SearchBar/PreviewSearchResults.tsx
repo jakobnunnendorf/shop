5e2a8743755dad192992cf221ed90c73e5ed2d56
“Jakob <jnunnendorf@icloud.com>
2023-08-13 11:35:20 +0800

    refactor(previewsearchResults): add text search logic here

--- src/app/Header/SearchBar/PreviewSearchResults.tsx
+++ src/app/Header/SearchBar/PreviewSearchResults.tsx
@@ -1,8 +1,10 @@
 'use client';
+
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { FiArrowRight } from 'react-icons/fi';
 import Expanded from '@components/ProductCard/Expanded/Expanded';
 import {
@@ -15,15 +17,36 @@ import {
 } from '@globalState/SearchResults';
 
 export default function PreviewSearchResults() {
+    const supabase = createClientComponentClient();
     const isShopPage = usePathname() === '/shop';
 
     const { searchFilter } = useContext(
         ActiveFiltersContext
     ) as FilterContextType;
 
-    const { searchResults } = useContext(
+    const { searchResults, setSearchResults } = useContext(
         SearchResultsContext
     ) as SearchResultsContextType;
+
+    useEffect(() => {
+        if (searchFilter !== '') {
+            const getProducts = async () => {
+                const getSearchProducts = async () => {
+                    const { data: searchResults } = await supabase
+                        .from('products')
+                        .select('*')
+                        .textSearch('title', searchFilter)
+                        .limit(3);
+
+                    return searchResults;
+                };
+
+                const products = (await getSearchProducts()) as product[];
+                setSearchResults(products);
+            };
+            getProducts();
+        }
+    }, [searchFilter, supabase, setSearchResults]);
 
     const topThreeResults = searchResults.slice(0, 3);
     const [selectedProduct] = useState<product | null>(null);
@@ -35,7 +58,7 @@ export default function PreviewSearchResults() {
                 !isShopPage
                     ? 'fixed'
                     : 'hidden'
-            } left-0 top-32 h-fit w-full bg-white`}
+            } left-0 top-12 h-fit w-full bg-white`}
         >
             <ul className='pt-4 space-y-2'>
                 {topThreeResults.map((searchResult, index) => {
