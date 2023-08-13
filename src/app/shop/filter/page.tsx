b790d989245f07f25650d97363f92972f1212955
“Jakob <jnunnendorf@icloud.com>
2023-08-13 17:33:49 +0800

    refactor(shop): move mobile filters to new page

---
+++ src/app/shop/filter/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from 'next';
+import MobileFilters from '../FilterBar/MobileFilters/MobileFilters';
+
+export const metadata = {
+    title: '',
+    description: '',
+};
+
+export default function MobileFilterPage() {
+    return (
+        <section className='w-full '>
+            <MobileFilters />
+        </section>
+    );
+}
--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from 'next/link';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { useContext, useEffect } from 'react';
 import ProductCard from '@components/ProductCard/ProductCard';
@@ -78,14 +79,24 @@ export default function ShopPage() {
     ]);
 
     const section = (
-        <section className='grid w-full grid-cols-2 gap-0 lg:w-fit lg:grid-cols-5 lg:gap-4 lg:p-4'>
-            {searchResults?.map((product, index) => {
-                return (
-                    <div key={index} className='w-full pb-4'>
-                        <ProductCard product={product} grid={true} />
-                    </div>
-                );
-            })}
+        <section>
+            <div className='grid place-content-center py-16'>
+                <Link
+                    href='/shop/filter'
+                    className='boarder boarder-coastal-blue-10 rounded-full border border-coastal-blue-10 px-4 py-2 text-xl font-bold text-coastal-blue-10'
+                >
+                    Filter auswählen
+                </Link>
+            </div>
+            <ul className='grid w-full grid-cols-2 gap-0 lg:w-fit lg:grid-cols-5 lg:gap-4 lg:p-4'>
+                {searchResults?.map((product, index) => {
+                    return (
+                        <li key={index} className='w-full pb-4'>
+                            <ProductCard product={product} grid={true} />
+                        </li>
+                    );
+                })}
+            </ul>
         </section>
     );
     return section;
