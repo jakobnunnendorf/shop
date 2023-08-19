86c78bd607827c3809743e89f31c670d780deeb5
“Jakob <jnunnendorf@icloud.com>
2023-08-19 10:57:33 +0800

    feat(help page): add banner

--- src/app/Hilfe/page.tsx
+++ src/app/Hilfe/page.tsx
@@ -12,14 +12,16 @@ import './Help.css';
 export default function Help() {
     return (
         <div className='help'>
-            <figure className='relative'>
-                <Image
-                    src='https://dominionpayroll.com/wp-content/uploads/HelpCenter.png'
-                    alt='Hilfezentrum'
-                    className='help__image'
-                    fill={true}
-                />
-            </figure>
+            <div
+                style={{
+                    backgroundImage: "url('/three_phones_v2_cropped.png')",
+                }}
+                className='relative grid w-full bg-center bg-no-repeat bg-cover place-content-center h-fit rborder'
+            >
+                <h2 className='z-10 py-16 text-5xl font-bold text-center lg:py-32 gradient-text lg:text-9xl text-coastal-blue-10 '>
+                    Hilfe Center
+                </h2>
+            </div>
             <div className='help__section'>
                 <FAQ />
             </div>
