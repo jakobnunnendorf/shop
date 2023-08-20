a803c831604be06c8321f8f077ff9a94fb8e8a3c
“Jakob <jnunnendorf@icloud.com>
2023-08-20 13:53:22 +0800

    feat(hilfe): add link to ds

--- src/app/Hilfe/PrivacyPolicy/PrivacyPolicy.js
+++ src/app/Hilfe/PrivacyPolicy/PrivacyPolicy.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 import './PrivacyPolicy.css';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -5,11 +6,21 @@ import './PrivacyPolicy.css';
 
 export default function PrivacyPolicy() {
     return (
-        <iframe
-            title='ds'
-            src='https://www.phone2door.com/hilfe/datenschutz'
-            width='600'
-            height='400'
-        ></iframe>
+        <div className='w-full'>
+            <h2 className='py-16 text-3xl text-center'>
+                Datenschutz
+                <Link href='https://www.phone2door.com/Hilfe/datenschutz'>
+                    <span className='text-lg font-bold text-blue-800 underlined'>
+                        &nbsp;(Mehr erfahren)
+                    </span>
+                </Link>
+            </h2>
+            <iframe
+                title='ds'
+                src='https://www.phone2door.com/Hilfe/datenschutz'
+                width='100%'
+                height='400'
+            ></iframe>
+        </div>
     );
 }
