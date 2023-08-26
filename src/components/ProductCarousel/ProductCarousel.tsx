546dc1e558951028b3485af89e020aa5fdb6540a
“Jakob <jnunnendorf@icloud.com>
2023-08-26 18:52:43 +0800

    refactor(productcarousel): make server component

--- src/components/ProductCarousel/ProductCarousel.tsx
+++ src/components/ProductCarousel/ProductCarousel.tsx
@@ -1,22 +1,33 @@
 // src/components/ProductCarousel/ProductCarousel.tsx
-'use client';
 import React from 'react';
 import ProductCard from '@components/ProductCard/ProductCard';
+import { fetchProductsFromCategory } from '@lib/helperFunctions';
 
-export function ProductCarousel({
+export async function ProductCarousel({
     heading,
-    productData,
+    productCategory,
+    amountOfProducts = 10,
 }: {
     heading: string;
-    productData: product[];
+    productCategory?: string;
+    amountOfProducts?: number;
 }) {
+    const products = await fetchProductsFromCategory(
+        amountOfProducts,
+        productCategory
+    );
+
     return (
-        <div className='flex flex-col items-center py-8 lg:py-16 '>
+        <div
+            className={`flex-col items-center py-8 lg:py-16 ${
+                products.length > 0 ? 'flex' : 'hidden'
+            }`}
+        >
             <h2 className='m-8 text-5xl font-bold gradient-text text-coastal-blue-10 hover:underline'>
                 {heading}
             </h2>
             <ul className='flex px-8 pb-8 space-x-4 overflow-x-auto h-fit w-96 snap-x scrollbar-hide lg:w-2/3'>
-                {productData?.map((product, index) => {
+                {products?.map((product, index) => {
                     return (
                         <li className=' snap-center' key={index}>
                             <div className='w-48 lg:w-64'>
--- src/lib/helperFunctions.ts
+++ src/lib/helperFunctions.ts
@@ -14,6 +14,91 @@ interface uploadProduct {
     imageURL_object: imageURL_object;
 }
 
+export const fetchProductsFromCategory = async (
+    amount: number,
+    category?: string
+) => {
+    if (category) {
+        const { data } = await supabase
+            .from('products')
+            .select('*')
+            .eq('category', category)
+            .limit(amount );
+        return data as product[];
+    } else {
+        const { data } = await supabase
+            .from('products')
+            .select('*')
+            .limit(amount);
+        return data as product[];
+    }
+};
+
+type categoryType = [string, productCategory][];
+export const productCategories: categoryType = [
+    ['Handyhüllen', 'phone case'],
+    ['Panzergläser', 'screen protector'],
+    ['Ladekabel', 'charging cable'],
+    ['Ladestecker', 'charging adapter'],
+    ['Tablet-Taschen', 'tablet case'],
+    ['Handy-Halterungen', 'phone holder'],
+];
+
+export const fetchProductsByTextSearch = async (
+    textSearch: string,
+    amount?: number
+) => {
+    const { data } = (await supabase
+        .from('products')
+        .select('*')
+        .textSearch('title', textSearch)
+        .limit(amount ? amount : 30)) as sb_fetchResponseObject<product[]>;
+    return data;
+};
+
+export const filterProductArrayByDeviceFilters = (
+    products: product[],
+    deviceFilters: device[]
+) => {
+    if (deviceFilters.length === 0) return products;
+    // first we filter all products
+    const filteredProducts = products.filter((product: product) => {
+        // in the filter we loop through all device filters
+        return deviceFilters.some((deviceFilter: device) => {
+            // if the device filter has no name, we only filter by brand
+            if (deviceFilter.name.length === 0) {
+                return product.compatibleModels?.some((compatibleDevice) => {
+                    return compatibleDevice.brand === deviceFilter.brand;
+                });
+            } else {
+                return product.compatibleModels?.some((compatibleDevice) => {
+                    return (
+                        compatibleDevice.brand === deviceFilter.brand &&
+                        compatibleDevice.name === deviceFilter.name
+                    );
+                });
+            }
+        });
+    });
+    return filteredProducts;
+};
+
+export const filterProductArrayByPriceFilters = (
+    products: product[],
+    priceFilters: number[][]
+) => {
+    if (priceFilters.length === 0) return products;
+    const filteredProducts = products.filter((product: product) => {
+        return priceFilters.some((priceFilter: number[]) => {
+            return (
+                product.price >= priceFilter[0] &&
+                product.price <= priceFilter[1]
+            );
+        });
+    });
+    return filteredProducts;
+};
+
 
 export async function uploadFile_to_supabase_storage(
     bucket: string,
