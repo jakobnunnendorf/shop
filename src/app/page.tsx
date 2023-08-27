f2738933d408bd550b23d5ccb314ea06ebf99aae
“Jakob <jnunnendorf@icloud.com>
2023-08-27 20:24:11 +0800

    refactor(home): remove unecessary supabase client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { Metadata } from 'next';
-import { cookies } from 'next/headers';
 import { CategoryBlocks } from '@components/CategoryBlocks/CategoryBlocks';
 import { ProductCarousel } from '@components/ProductCarousel/ProductCarousel';
 import Home from './Home/Home';
@@ -13,8 +11,6 @@ export const metadata: Metadata = {
 };
 
 export default async function HomePage() {
-    const supabase = createServerComponentClient({ cookies });
-
     return (
         <main className='w-full'>
             <Home />
