b9c74e461cafad49d95fb085c571e093421ef256
“Jakob <jnunnendorf@icloud.com>
2023-08-27 20:16:04 +0800

    refactor(categoryblocks): add links

--- src/components/CategoryBlocks/CategoryBlock.tsx
+++ src/components/CategoryBlocks/CategoryBlock.tsx
@@ -1,5 +1,8 @@
+import Link from 'next/link';
+
 interface CategoryBlockProps {
     name: string;
+    category: string | null;
     description: string;
     icon: React.ReactElement;
     style: string;
@@ -10,15 +13,19 @@ export function CategoryBlock({
 }: {
     categoryData: CategoryBlockProps;
 }) {
+    console.log(categoryData);
     return (
-        <div
-            className={`flex aspect-square flex-col items-center justify-around rounded-xl border-5  ${categoryData.style} py-6 text-center`}
+        <Link
+            href={{
+                pathname: '/shop',
+                query: { category: categoryData.category },
+            }}
+            className={` flex aspect-square flex-col items-center justify-around rounded-xl border-5  ${categoryData.style} py-6 text-center`}
         >
             <div className='space-y-2 font-bold'>
                 {categoryData.icon}
                 <h3>{categoryData.name}</h3>
-                {/* <p>{categoryData.description}</p> */}
             </div>
-        </div>
+        </Link>
     );
 }
--- src/components/CategoryBlocks/Left.tsx
+++ src/components/CategoryBlocks/Left.tsx
@@ -10,11 +10,11 @@ import { CategoryBlock } from './CategoryBlock';
 
 export default function Left() {
     return (
-        <aside className='space-y-8 bg-coastal-blue-10 px-10 py-8 lg:py-16'>
-            <h1 className='text-center text-5xl font-bold text-sandy-beige-10'>
+        <aside className='px-10 py-8 space-y-8 bg-coastal-blue-10 lg:py-16'>
+            <h1 className='text-5xl font-bold text-center text-sandy-beige-10'>
                 Entdecke unsere Produkte
             </h1>
-            <div className='grid w-fit grid-cols-3 gap-4'>
+            <div className='grid grid-cols-3 gap-4 w-fit'>
                 {Object.values(categories).map((category, index) => (
                     <CategoryBlock key={index} categoryData={category} />
                 ))}
@@ -26,38 +26,44 @@ export default function Left() {
 const categories = {
     category1: {
         name: 'Handyhüllen',
+        category: 'phone case',
         description: 'Schütze dein Handy mit einer Hülle',
-        icon: <FiSmartphone className='m-auto h-8 w-8' />,
+        icon: <FiSmartphone className='w-8 h-8 m-auto' />,
         style: 'text-multicolore-pink border-multicolore-pink',
     },
     category2: {
         name: 'Panzergläser',
+        category: 'screen protector',
         description: 'Hochwertige Panzergläser für optimalen Schutz',
-        icon: <FiShield className='m-auto h-8 w-8' />,
+        icon: <FiShield className='w-8 h-8 m-auto' />,
         style: 'text-multicolore-orange border-multicolore-orange',
     },
     category3: {
         name: 'Ladekabel',
+        category: 'charging cable',
         description: 'Hochwertige Ladekabel für zuverlässiges Aufladen',
-        icon: <FiBatteryCharging className='m-auto h-8 w-8' />,
+        icon: <FiBatteryCharging className='w-8 h-8 m-auto' />,
         style: 'text-multicolore-yellow border-multicolore-yellow',
     },
     category4: {
         name: 'Tablet Taschen',
+        category: 'tablet case',
         description: 'Schütze dein Tablet mit stilvollen Taschen',
-        icon: <FiTablet className='m-auto h-8 w-8' />,
+        icon: <FiTablet className='w-8 h-8 m-auto' />,
         style: 'text-multicolore-green border-multicolore-green',
     },
     category5: {
         name: 'Handy Halterungen',
+        category: 'phone holder',
         description: 'Praktische Halterungen für dein Handy',
-        icon: <FiSmartphone className='m-auto h-8 w-8' />,
+        icon: <FiSmartphone className='w-8 h-8 m-auto' />,
         style: 'text-multicolore-lightblue border-multicolore-lightblue',
     },
     category6: {
         name: 'Sonstiges',
+        category: null,
         description: 'Verschiedenes Zubehör für deine Geräte',
-        icon: <FiMoreHorizontal className='m-auto h-8 w-8' />,
+        icon: <FiMoreHorizontal className='w-8 h-8 m-auto' />,
         style: 'text-multicolore-blue border-multicolore-blue',
     },
 };
