b913e3f65e52469c11b79331fe60f42ab9dc7010
“Jakob <jnunnendorf@icloud.com>
2023-12-18 11:39:11 +0800

    refactor(Footer): rm social & add contact

--- src/app/Footer/Footer.tsx
+++ src/app/Footer/Footer.tsx
@@ -6,13 +6,14 @@ export default function Footer() {
     return (
         <div className=' max-w-screen'>
             <footer className='text-center bg-sandy-beige-4 text-coastal-blue-10 dark:bg-neutral-600 dark:text-neutral-200 lg:text-left'>
+                {/* TODO: Add social media links
                 <div className='flex items-center justify-center p-6 border-b-2 border-neutral-200 dark:border-neutral-500 lg:justify-between'>
                     <div className='hidden mr-12 lg:block'>
                         <span>Finde uns auf Social Media</span>{' '}
                     </div>
                     <div className='flex justify-center'>
                         <Link
-                            href='#!' // TODO: Add social media links
+                            href='#!'
                             className='mr-6 text-neutral-600 dark:text-neutral-200'
                         >
                             <svg
@@ -39,6 +40,7 @@ export default function Footer() {
                         </Link>
                     </div>
                 </div>
+    */}
 
                 <div className='py-10 mx-6 text-center md:text-left'>
                     <div className='grid gap-8 grid-1 md:grid-cols-2 lg:grid-cols-4'>
@@ -172,7 +174,7 @@ export default function Footer() {
                                     <path d='M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z' />
                                     <path d='M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z' />
                                 </svg>
-                                Köln...
+                                Horbellerstr. 9, 50858 Köln
                             </p>
                             <p className='flex items-center justify-center mb-4 md:justify-start'>
                                 <svg
@@ -184,7 +186,7 @@ export default function Footer() {
                                     <path d='M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z' />
                                     <path d='M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z' />
                                 </svg>
-                                info@example.com
+                                info@phone2door.com
                             </p>
                             <p className='flex items-center justify-center mb-4 md:justify-start'>
                                 <svg
@@ -199,22 +201,7 @@ export default function Footer() {
                                         clipRule='evenodd'
                                     />
                                 </svg>
-                                + telefonnummer
-                            </p>
-                            <p className='flex items-center justify-center md:justify-start'>
-                                <svg
-                                    xmlns='http://www.w3.org/2000/svg'
-                                    viewBox='0 0 24 24'
-                                    fill='currentColor'
-                                    className='w-5 h-5 mr-3'
-                                >
-                                    <path
-                                        fillRule='evenodd'
-                                        d='M7.875 1.5C6.839 1.5 6 2.34 6 3.375v2.99c-.426.053-.851.11-1.274.174-1.454.218-2.476 1.483-2.476 2.917v6.294a3 3 0 003 3h.27l-.155 1.705A1.875 1.875 0 007.232 22.5h9.536a1.875 1.875 0 001.867-2.045l-.155-1.705h.27a3 3 0 003-3V9.456c0-1.434-1.022-2.7-2.476-2.917A48.716 48.716 0 0018 6.366V3.375c0-1.036-.84-1.875-1.875-1.875h-8.25zM16.5 6.205v-2.83A.375.375 0 0016.125 3h-8.25a.375.375 0 00-.375.375v2.83a49.353 49.353 0 019 0zm-.217 8.265c.178.018.317.16.333.337l.526 5.784a.375.375 0 01-.374.409H7.232a.375.375 0 01-.374-.409l.526-5.784a.373.373 0 01.333-.337 41.741 41.741 0 018.566 0zm.967-3.97a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H18a.75.75 0 01-.75-.75V10.5zM15 9.75a.75.75 0 00-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 00.75-.75V10.5a.75.75 0 00-.75-.75H15z'
-                                        clipRule='evenodd'
-                                    />
-                                </svg>
-                                + telefonnummer
+                                +49 2237 6036167
                             </p>
                         </div>
                     </div>
